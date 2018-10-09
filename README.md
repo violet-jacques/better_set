41256054b477a8d57cb102c3ce05fd446147b0aa
Zoey Jacques <zoey.jacques630@gmail.com>
2018-10-09 19:44:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#BetterSet
+
+The set class in ruby doesnt really act like a set, this set class does
---
+++ spec/better_set_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe BetterSet do
+  it "has a version number" do
+    expect(BetterSet::VERSION).not_to be nil
+  end
+
+  describe ".new" do
+    it "initializes a MikeSet" do
+      expect(BetterSet.new).to be_a(BetterSet::MikeSet)
+    end
+  end
+
+  describe ".[]" do
+    it "initializes a MikeSet" do
+      expect(BetterSet.new).to be_a(BetterSet::MikeSet)
+    end
+  end
+end
