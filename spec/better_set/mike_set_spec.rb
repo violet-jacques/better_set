b5171edf4f9ad9604b0b03dfc90f403de72d4f05
Zoey Jacques <zoey.jacques630@gmail.com>
2018-10-09 21:39:38 -0400

    add superset, proper_superset and ==

--- spec/better_set/mike_set_spec.rb
+++ spec/better_set/mike_set_spec.rb
@@ -40,6 +40,48 @@ module BetterSet
       end
     end
 
+    describe "#==" do
+      context "self is a subset of other" do
+        context "self is a superset of other" do
+          it "returns true" do
+            set = MikeSet.new
+            other = MikeSet.new
+
+            expect(set == other).to be(true)
+          end
+
+          context "self is not a superset of other" do
+            it "returns false" do
+              set = MikeSet.new
+              other = MikeSet.new(["justine"])
+
+              expect(set == other).to be(false)
+            end
+          end
+        end
+      end
+
+      context "self is a superset of other" do
+        context "self is not a super set of other" do
+          it "returns false" do
+            set = MikeSet.new
+            other = MikeSet.new(["justine"])
+
+            expect(set == other).to be(false)
+          end
+        end
+      end
+
+      context "self and other are disjoint" do
+        it "returns false" do
+          set = MikeSet.new(["mike"])
+          other = MikeSet.new(["justine"])
+
+          expect(set == other).to be(false)
+        end
+      end
+    end
+
     describe "#subset?" do
       context "other is not a set" do
         it "raises an argument error" do
@@ -70,6 +112,15 @@ module BetterSet
           other = MikeSet.new(["hey", "dawg"])
 
           expect(set.subset?(other)).to be(true)
+        end
+      end
+
+      context "all of the elements in other are in self" do
+        it "returns false" do
+          other = MikeSet.new(["hey"])
+          set = MikeSet.new(["hey", "dawg"])
+
+          expect(set.subset?(other)).to be(false)
         end
       end
 
@@ -139,6 +190,15 @@ module BetterSet
         end
       end
 
+      context "all of the elements in other are in self" do
+        it "returns false" do
+          other = MikeSet.new(["hey"])
+          set = MikeSet.new(["hey", "dawg"])
+
+          expect(set.proper_subset?(other)).to be(false)
+        end
+      end
+
       context "at least one of the elements in self is not in other" do
         it "returns false" do
           set = MikeSet.new(["yo"])
@@ -149,13 +209,134 @@ module BetterSet
         end
       end
 
-      context "self is a subset of other and the same length as other" do
+      context "self is a subset of other and a superset" do
         it "returns false" do
           set = MikeSet.new(["yo"])
 
           other = MikeSet.new(["yo"])
 
           expect(set.proper_subset?(other)).to be(false)
+        end
+      end
+    end
+
+    describe "#superset?" do
+      context "other is not a set" do
+        it "raises an argument error" do
+          set = MikeSet.new
+          other = "hey"
+
+          expect { set.superset?(other) }.to raise_error(
+            ArgumentError,
+            "Argument must be a BetterSet",
+          )
+        end
+      end
+
+      context "self is the empty set" do
+        it "returns false" do
+          set = MikeSet.new
+          other = MikeSet.new(["hey"])
+
+          expect(set.superset?(other)).to be(false)
+        end
+      end
+
+      context "all of the elements in self are in other" do
+        it "returns false" do
+          set = MikeSet.new(["hey"])
+          other = MikeSet.new(["hey", "dawg"])
+
+          expect(set.superset?(other)).to be(false)
+        end
+      end
+
+      context "all of the elements in other are in self" do
+        it "returns false" do
+          other = MikeSet.new(["hey"])
+          set = MikeSet.new(["hey", "dawg"])
+
+          expect(set.superset?(other)).to be(true)
+        end
+      end
+
+      context "self is the same as other" do
+        it "returns true" do
+          set = MikeSet.new(["hey"])
+          other = MikeSet.new(["hey"])
+
+          expect(set.superset?(other)).to be(true)
+        end
+      end
+
+      context "at least one of the elements in other is not in self" do
+        it "returns false" do
+          set = MikeSet.new(["yo", "hey"])
+          other = MikeSet.new(["dawg", "yo"])
+
+          expect(set.superset?(other)).to be(false)
+        end
+      end
+    end
+
+    describe "#proper_superset?" do
+      context "other is not a set" do
+        it "raises an argument error" do
+          set = MikeSet.new
+          other = "hey"
+
+          expect { set.proper_superset?(other) }.to raise_error(
+            ArgumentError,
+            "Argument must be a BetterSet",
+          )
+        end
+      end
+
+      context "self is the empty set" do
+        context "other is not the empty set" do
+          it "returns false" do
+            set = MikeSet.new
+            other = MikeSet.new(["hey"])
+
+            expect(set.proper_superset?(other)).to be(false)
+          end
+        end
+
+        context "other is the empty set" do
+          it "returns false" do
+            set = MikeSet.new
+            other = MikeSet.new
+
+            expect(set.proper_superset?(other)).to be(false)
+          end
+        end
+      end
+
+      context "all of the elements in other are in self" do
+        it "returns true" do
+          other = MikeSet.new(["hey"])
+          set = MikeSet.new(["hey", "dawg"])
+
+          expect(set.proper_superset?(other)).to be(true)
+        end
+      end
+
+      context "at least one of the elements in other is not in self" do
+        it "returns false" do
+          set = MikeSet.new(["yo", "hey", "son"])
+          other = MikeSet.new(["dawg", "yo"])
+
+          expect(set.proper_superset?(other)).to be(false)
+        end
+      end
+
+      context "self is a superset of other and a subset of other" do
+        it "returns false" do
+          set = MikeSet.new(["yo"])
+
+          other = MikeSet.new(["yo"])
+
+          expect(set.proper_superset?(other)).to be(false)
         end
       end
     end
