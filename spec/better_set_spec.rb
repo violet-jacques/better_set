a4692cb2014b4f140588b58580130cc4709b53f5
Zoey Jacques <zoey.jacques630@gmail.com>
2018-10-11 11:31:03 -0400

    refactor class methods of better set module to take array

--- spec/better_set_spec.rb
+++ spec/better_set_spec.rb
@@ -5,13 +5,19 @@ RSpec.describe BetterSet do
 
   describe ".new" do
     it "initializes a MikeSet" do
+      array = [1,2,3]
+
       expect(BetterSet.new).to be_a(BetterSet::MikeSet)
+
+      expect(BetterSet.new(array)).to eq(BetterSet::MikeSet.new(array))
     end
   end
 
   describe ".[]" do
     it "initializes a MikeSet" do
       expect(BetterSet[]).to be_a(BetterSet::MikeSet)
+
+      expect(BetterSet[1,2,3]).to eq(BetterSet::MikeSet.new([1,2,3]))
     end
   end
 end
