8d04b8a9b21de1a1c43c998f7f9f83ca4b394f24
Zoey Jacques <zoey.jacques630@gmail.com>
2018-10-23 19:04:57 -0400

    with the addition of other structures these are no longer relevant

--- lib/better_set.rb
+++ lib/better_set.rb
@@ -2,11 +2,4 @@ require "better_set/version"
 require "better_set/set"
 
 module BetterSet
-  def self.[](*array)
-    self.new(array)
-  end
-
-  def self.new(array = [])
-    Set.new(array)
-  end
 end
