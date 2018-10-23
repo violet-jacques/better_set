ca3faf8e3475f67e9370d46999bb73c1b9ded235
Violet Jacques <violet.jacques630@gmail.com>
2018-10-23 18:56:58 -0400

    support ordered pairs

---
+++ lib/better_set/values/initial_ordered_pair_array.rb
@@ -0,0 +1,28 @@
+module BetterSet
+  module Values
+    class InitialOrderedPairArray
+      def self.value(first, second)
+        new(first, second).value
+      end
+
+      def initialize(first, second)
+        @first = first
+        @second = second
+      end
+
+      def value
+        [first_to_set, second_to_set]
+      end
+
+      private
+
+      def first_to_set
+        Set.new([@first])
+      end
+
+      def second_to_set
+        Set.new([@first, @second])
+      end
+    end
+  end
+end
