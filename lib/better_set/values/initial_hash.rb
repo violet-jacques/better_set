d2940620a0e6664021066cb9784b56352273db79
Violet Jacques <violet.jacques630@gmail.com>
2018-10-23 16:33:55 -0400

    fix duplicate set members when member is itself a set

---
+++ lib/better_set/values/initial_hash.rb
@@ -0,0 +1,45 @@
+module BetterSet
+  module Values
+    class InitialHash
+      NOT_ARRAY_ERROR_MESSAGE = "Argument must be Array class".freeze
+
+      def self.value(array)
+        new(array).value
+      end
+
+      def initialize(array)
+        @array = array
+      end
+
+      def value
+        raise_not_arrray_error unless initialized_with_array?
+
+        create_hash
+      end
+
+      private
+
+      def create_hash
+        @array.reduce(Hash.new(false)) do |memo, element|
+          add_unique_elements_to_hash(memo, element)
+        end
+      end
+
+      def add_unique_elements_to_hash(hash, element)
+        if hash.keys.include?(element)
+          hash
+        else
+          hash.merge(element => true)
+        end
+      end
+
+      def initialized_with_array?
+        @array.class == Array
+      end
+
+      def raise_not_arrray_error
+        raise ArgumentError, NOT_ARRAY_ERROR_MESSAGE
+      end
+    end
+  end
+end
